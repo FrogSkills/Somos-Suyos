ea255259f1934e0fa41c74247753a2296effebc0
Miguel <mgamez20@icloud.com>
2025-12-31 18:26:36 -0500

    Starting the somos suyos app

---
+++ SOS/Videos.swift
@@ -0,0 +1,18 @@
+//
+//  Videos.swift
+//  SOS
+//
+//  Created by Miguel on 12/31/25.
+//
+
+import SwiftUI
+
+struct Videos: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Videos()
+}
