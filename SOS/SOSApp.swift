8205a9cfa5b295955559d19ea23cb3b2b033ac23
Miguel <mgamez20@icloud.com>
2025-12-31 18:23:01 -0500

    Initial Commit

---
+++ SOS/SOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SOSApp.swift
+//  SOS
+//
+//  Created by Miguel on 12/31/25.
+//
+
+import SwiftUI
+
+@main
+struct SOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
